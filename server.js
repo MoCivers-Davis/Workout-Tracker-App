41b358c3c8f8dff21e6ea1c8b9420f906798d7d9
Mo Civ <mocivers@comcast.net>
2020-03-27 22:45:57 -0400

    updated changes

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+
+
+
 const PORT = process.env.PORT || 3000; //Setting the PORT
 
 const db = require("./models");
 
 
 const app = express();
+
+
 
 app.use(logger("dev")); //see external npm's above.  Morgan is used for loggig request detials.
 //This is also telling express "app" to "use" morgan as "logger" and morgan to log in the combined
@@ -20,14 +25,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-//workout is the name of the database
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_w4x4kjln:6m0b62nk95u80l08mb3079khdn@ds027409.mlab.com:27409/heroku_w4x4kjln", { useNewUrlParser: true });
+//workout is the name of the database  
 //When we deploy homework to Heroku we want be able to use localhost.  So we have to use "mongoose.connect(process.env.MONGODB_URI" as illustrated above 
 
 app.use(require("./routes/view.js"));///requiring the view.js in the routes folder as this is where my routes are located
 app.use(require("./routes/api.js"));///requiring the view.js in the routes folder as this is where my routes are located
 
+
 // Listen on port 3000
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("App running on port 3000!");
   });
